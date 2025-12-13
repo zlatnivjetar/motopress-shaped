9a7935f20b7bcea8b2bb6f6c74c962108d0db38b
David Marinović <marinovic.business@gmail.com>
2025-12-13 09:33:05 +0100

    Update view-details-button.php

--- templates/loop-room-type/view-details-button.php
+++ templates/loop-room-type/view-details-button.php
@@ -10,14 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 do_action( 'mphb_render_loop_room_type_before_view_details_button' );
 ?>
 
-    <a href="<?php echo esc_url(get_permalink()); ?>" class="elementor-button-link elementor-button" role="button">
-        <span class="elementor-button-content-wrapper">
-            <span class="elementor-button-text">VIEW ROOM DETAILS</span>
-            <span class="elementor-button-icon elementor-align-icon-right">
-                <i class="opal-icon-long-arrow-right" aria-hidden="true"></i>
-            </span>
-        </span>
-    </a>
 
 
 <?php
